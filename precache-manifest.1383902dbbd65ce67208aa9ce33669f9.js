705493278f7c650817e68608df71fe667eda9cee
Vasyl Velmyk <velmyk@gmail.com>
2020-03-05 15:06:52 +0200

    Updates

---
+++ precache-manifest.1383902dbbd65ce67208aa9ce33669f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d90f9ce2bd8b95c9d01037c96549f342",
+    "url": "/spa/index.html"
+  },
+  {
+    "revision": "e051c5030584a0c34938",
+    "url": "/spa/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6c072d064dd61272b583",
+    "url": "/spa/static/js/2.184d5aec.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/spa/static/js/2.184d5aec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e051c5030584a0c34938",
+    "url": "/spa/static/js/main.d97852de.chunk.js"
+  },
+  {
+    "revision": "494daeec123a6ef5954d",
+    "url": "/spa/static/js/runtime-main.d9da1a79.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spa/static/media/logo.5d5d9eef.svg"
+  }
+]);
